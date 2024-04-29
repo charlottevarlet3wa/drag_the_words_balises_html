585b23187139df227296cf82bc8b727ffa7b8db6
Charlotte Varlet <c.varlet@3wa.fr>
2024-04-29 15:14:53 +0200

    styles

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
         shuffle(draggables);
         draggables.forEach(draggable => wordsContainer.appendChild(draggable));
-        container.innerHTML = modifiedText.replace(/\n/g, '<br>').replace(/ {5}/g, '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;');
+        container.innerHTML = modifiedText.replace(/\n/g, '<br>').replace(/ {5}/g, '&nbsp;&nbsp;');
         applyDraggableListeners();
         applyDroppableListeners();
       })
@@ -56,12 +56,28 @@ function shuffle(array) {
 }
 
 function encodeHtml(html) {
-  return html.replace(/&/g, '&amp;')
+  html = html.replace(/&/g, '&amp;')
              .replace(/</g, '&lt;')
              .replace(/>/g, '&gt;')
-             .replace(/"/g, '&quot;')
-             .replace(/'/g, '&#039;');
+            //  .replace(/"/g, '&quot;')
+            //  .replace(/'/g, '&#039;');
+
+  // Comment highlighting
+  html = html.replace(/&lt;!--(.*?)--&gt;/g, '<span class="comment">&lt;!--$1--&gt;</span>');
+  
+  // Tag and attribute highlighting
+  html = html.replace(/(&lt;)(\/?)([a-zA-Z0-9-]+)(.*?)(&gt;)/g, function(match, p1, p2, p3, p4, p5) {
+      return '<span class="angle-brackets">' + p1 + '</span>' + 
+             '<span class="tag">' + p2 + p3 + '</span>' + 
+             p4.replace(/([a-zA-Z-]+)(=)("[^"]*"|'[^']*')/g, '<span class="attribute-name">$1</span>$2<span class="attribute-value">$3</span>') + 
+             '<span class="angle-brackets">' + p5 + '</span>';
+  });
+  
+
+  return html;
 }
+
+
 
 
 document.querySelectorAll('.draggable').forEach(item => {
